b6d38cf7442e29a85ed8230bc126eed64edcad28
Maciej S. Szmigiero <mail@maciej.szmigiero.name>
2016-08-25 18:22:35 +0200

    Make minidriver buildable again on mingw
    Currently, minidriver build is broken on mingw. Let's make it work again.
    
    For this, include adapted cardmod-mingw-compat.h with few function argument
    decorations from Alon Bar-Lev's old build repository to make mingw build
    almost self-contained - still requires cardmod.h from CNG, however.
    
    Signed-off-by: Maciej S. Szmigiero <mail@maciej.szmigiero.name>

---
+++ src/minidriver/cardmod-mingw-compat.h
@@ -0,0 +1,28 @@
+#define __deref
+#define __deref_opt_inout_bcount_part_opt(x,y)
+#define __deref_opt_out_bcount(x)
+#define __deref_out_bcount(x)
+#define __deref_out_bcount_opt(x)
+#define __deref_out_ecount(x)
+#define __in
+#define __in_bcount(x)
+#define __in_bcount_opt(x)
+#define __in_ecount(x)
+#define __in_opt
+#define __inout
+#define __inout_bcount_full(x)
+#define __inout_bcount_opt(x)
+#define __out
+#define __out_bcount(x)
+#define __out_bcount_full(x)
+#define __out_bcount_part_opt(x,y)
+#define __out_bcount_part_opt(x,y)
+#define __out_ecount(x)
+#define __out_opt
+#define __struct_bcount(x)
+#define __success(x)
+#define _Printf_format_string_
+
+#ifndef NTE_BUFFER_TOO_SMALL
+#define NTE_BUFFER_TOO_SMALL _HRESULT_TYPEDEF_(0x80090028)
+#endif
